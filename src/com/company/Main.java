b91e04a2d1f2c5f95480b6d711bfee74725dbc49
anshultyag <you@example.com>
2021-12-25 23:53:15 +0530

    Added: Random number uc 03

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int start_position=0;
+        System.out.println(start_position);
+        int roll_dice = (int)(Math.floor(Math.random() *10) % 6) + 1;
+        System.out.println("Rolling Dice We Get: "+roll_dice);
+        int playerCheckOption = (int)(Math.random() * 3 ) + 1;
+
+
+            }
+}
