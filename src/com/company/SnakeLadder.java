15b19c86a979b3b668f195a63255ac19ac4a4c5e
anshultyag <you@example.com>
2021-12-26 00:00:33 +0530

    Added: No play Snake or ladder uc 03

--- src/com/company/SnakeLadder.java
+++ src/com/company/SnakeLadder.java
@@ -8,6 +8,24 @@ public class SnakeLadder {
     public static void main(String[] args) {
         int startPosition = 0;
         System.out.println("Starting Position : " + startPosition);
+        int roll_dice = (int) (Math.floor(Math.random() * 10) % 6) + 1;
+        System.out.println("Rolling dice We Got: " + roll_dice);
+        int playerCheck = (int) (Math.floor(Math.random() * 10) % 3) + 1;
+        switch (playerCheck) {
+            case no_play:
+                System.out.println("No Play! Your position can not be changed,you are at :  " + startPosition);
+                break;
+            case ladder:
+                startPosition = startPosition + roll_dice;
+                System.out.println("Congratulation!climb up the ladder:  " + startPosition);
+                break;
+            case snake:
+                startPosition = startPosition - roll_dice;
+                System.out.println("Oops! swallowed by snake  :  " + startPosition);
+                break;
+            default:
+                System.out.println("Something went wrong");
+        }
 
     }
 }
