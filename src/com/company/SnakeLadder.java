360aada0dfd1cf24c75adfc6d7be1bde1ff6becc
anshultyag <you@example.com>
2021-12-25 23:46:35 +0530

    Added: Snake and ladder uc 01

---
+++ src/com/company/SnakeLadder.java
@@ -0,0 +1,13 @@
+package com.company;
+public class SnakeLadder {
+    public static final int no_play = 1;
+    public static final int ladder = 2;
+    public static final int snake = 3;
+
+
+    public static void main(String[] args) {
+        int startPosition = 0;
+        System.out.println("Starting Position : " + startPosition);
+
+    }
+}
