96bd2dcd75853cb0b673a3526e867f0ea9fa6b94
anshultyag <you@example.com>
2021-12-26 13:13:55 +0530

    Added: Repeat till the Player reaches the winning position 100 uc 04

--- src/com/company/SnakeLadder.java
+++ src/com/company/SnakeLadder.java
@@ -3,29 +3,48 @@ public class SnakeLadder {
     public static final int no_play = 1;
     public static final int ladder = 2;
     public static final int snake = 3;
-
+    public  static final int win = 100;
 
     public static void main(String[] args) {
-        int startPosition = 0;
-        System.out.println("Starting Position : " + startPosition);
-        int roll_dice = (int) (Math.floor(Math.random() * 10) % 6) + 1;
-        System.out.println("Rolling dice We Got: " + roll_dice);
-        int playerCheck = (int) (Math.floor(Math.random() * 10) % 3) + 1;
-        switch (playerCheck) {
-            case no_play:
-                System.out.println("No Play! Your position can not be changed,you are at :  " + startPosition);
-                break;
-            case ladder:
-                startPosition = startPosition + roll_dice;
-                System.out.println("Congratulation!climb up the ladder:  " + startPosition);
-                break;
-            case snake:
-                startPosition = startPosition - roll_dice;
-                System.out.println("Oops! swallowed by snake  :  " + startPosition);
-                break;
-            default:
-                System.out.println("Something went wrong");
-        }
+        int positionOfPlayer = 0;
+        int dice= 0;
+        while (positionOfPlayer <= win) {
 
+            int roll_dice = (int) (Math.floor(Math.random() * 10) % 6) + 1;
+            System.out.println("Rolling dice We Got: " + roll_dice);
+            System.out.println("Starting Position : " + positionOfPlayer);
+            int playerOption = (int) (Math.floor(Math.random() * 10) % 3) + 1;
+
+            switch (playerOption) {
+                case no_play:
+                    System.out.println("No Play! Your position can not be changed,you are at :  " + positionOfPlayer);
+                    break;
+
+                case ladder:
+                    positionOfPlayer = positionOfPlayer + roll_dice;
+                    System.out.println("Congratulation!climb up the ladder:  " + positionOfPlayer);
+                    if (positionOfPlayer > win) {
+                        positionOfPlayer = positionOfPlayer + roll_dice;
+                    }
+                    if (positionOfPlayer == win) {
+                        System.out.println("Player won");
+                        System.out.println("Position Of Player : " + positionOfPlayer);
+                    }
+                    break;
+
+                case snake:
+                    if (positionOfPlayer <= 0) {
+                        positionOfPlayer = positionOfPlayer;
+                    } else {
+                        positionOfPlayer = positionOfPlayer - roll_dice;
+                        System.out.println("Oops! swallowed by snake  :  " + positionOfPlayer);
+                        break;
+                    }
+
+                default:
+                    System.out.println("Something went wrong");
+            }
+        System.out.println("Player current position :" + positionOfPlayer);
+        }
     }
 }
